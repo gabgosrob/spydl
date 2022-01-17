110f6efaa661eef63ca5cf211b82c039cb0e413b
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-01-17 11:03:59 -0500

    move functions and add error catching

--- spycharts.py
+++ spycharts.py
@@ -1,13 +1,32 @@
+import requests
+from typing import List
 from user import User
+from spypoint import Photo
+from utils import save_image_from_data
 
 def main() -> None:
 	username = input("username: ")
 	password = input("password: ")
-	user = User(username, password)
+	
+	try:
+		user = User(username, password)
+		photos = user.get_all_photos()
+		print("Downloading...")
+		download_all_photos(photos)
+		print("Done!")
+	except Exception as e:
+		print("Something went wrong. Check your login info or try again later.")
 
-	photos = user.get_all_photos()
+def download_all_photos(photos: List[Photo]):
 	for photo in photos:
-		user.download_photo(photo)
+		download_photo(photo)
+
+def download_photo(photo: Photo) -> None:
+	data = get_photo_data(photo)
+	save_image_from_data(data, photo.originName)
+
+def get_photo_data(photo: Photo) -> bytes:
+	return requests.get(photo.url()).content
 
 if __name__ == "__main__":
 	main()
